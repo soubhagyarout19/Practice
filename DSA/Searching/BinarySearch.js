b9fb0968e2b94de82f8f00dc0cfb637c2bfc1422
Soubhagya Darshan Rout <soubhagya.darshanrout@clouwood.com>
2024-08-29 23:02:39 +0530

    BinarySearch

---
+++ DSA/Searching/BinarySearch.js
@@ -0,0 +1,22 @@
+let k = 4;
+let arr= [1, 2, 3, 4, 5] ;
+
+let start = 0
+let end = arr.length-1;
+
+while(start<=end){
+    let mid = Math.trunc((start+end)/2);
+    if(k>mid){
+        start = start + 1;
+    }
+    if(k<mid){
+        end = end - 1;
+    }
+    if(k === arr[mid]){
+        console.log(k + " is present in the array");
+        break;
+    }
+}
+
+// Time Complexity: O(log n), where n is the size of the given array
+// Auxiliary Space: O(1) , as we are not using any extra space
--- DSA/Searching/linearSearch.js
+++ DSA/Searching/linearSearch.js
@@ -11,3 +11,7 @@ const searchElement = (arg) => {
 
 let output = searchElement(10);
 console.log(output);
+
+// Best Case: In the best case, the key might be present at the first index. So the best case complexity is O(1)
+// Worst Case: In the worst case, the key might be present at the last index i.e., opposite to the end from which the search has started in the list. So the worst-case complexity is O(N) where N is the size of the list.
+// Average Case: O(N)
