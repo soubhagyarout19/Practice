3cef40713c9dbd3a9db8612ffb224c76e8934af6
Soubhagya Darshan Rout <soubhagya.darshanrout@clouwood.com>
2024-08-30 20:39:33 +0530

    missingNumber

---
+++ DSA/Searching/missingNumber.js
@@ -0,0 +1,15 @@
+const arr = [2,1,3,5,6,8,7,9,10];
+
+const missingNumber = (arr) => {
+    let sortedArray = arr.sort((a,b)=>{
+        return a - b;
+    })
+    for(let i=1; i<=arr.length; i++){
+        if(sortedArray[i-1] !== i){
+            return i;
+        }
+    }
+}
+
+let result = missingNumber(arr);
+console.log(result);
